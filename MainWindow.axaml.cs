d23dfd196b4e0c57b545946440480be4b3eddbad
Ari Cummings <ari@aricummings.com>
2025-07-07 23:00:28 -0400

    working!!!!

--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -6,96 +6,89 @@ namespace dotnetCalculator;
 
 public partial class MainWindow : Window
 {
+    private string currentNumber = "";
+    private double firstNumber = 0;
+    private string currentOperation = "";
+    private bool isNewNumber = true;
+
     public MainWindow()
     {
         InitializeComponent();
     }
 
-    public void calculate(int num1, int num2, int operation)
+    private void NumberButton_Click(object sender, RoutedEventArgs e)
     {
-        double result = 0;
+        if (sender is not Button button || button.Content is not string number)
+            return;
 
+        if (isNewNumber)
+        {
+            currentNumber = number;
+            isNewNumber = false;
+        }
+        else
+        {
+            currentNumber += number;
+        }
 
+        if (output != null)
+        {
+            output.Content = currentNumber;
+        }
+    }
 
+    private void OperationButton_Click(object sender, RoutedEventArgs e)
+    {
+        if (sender is not Button button)
+            return;
 
+        if (!string.IsNullOrEmpty(currentNumber))
+        {
+            if (!string.IsNullOrEmpty(currentOperation))
+            {
+                CalculateResult();
+            }
+            
+            firstNumber = double.Parse(currentNumber);
+            currentOperation = button.Tag?.ToString() ?? "";
+            isNewNumber = true;
+        }
+    }
 
+    private void EqualButton_Click(object sender, RoutedEventArgs e)
+    {
+        if (!string.IsNullOrEmpty(currentOperation) && !string.IsNullOrEmpty(currentNumber))
+        {
+            CalculateResult();
+            currentOperation = "";
+        }
+    }
+
+    private void CalculateResult()
+    {
+        if (string.IsNullOrEmpty(currentNumber) || output == null) return;
+
+        double secondNumber = double.Parse(currentNumber);
+        double result = currentOperation switch
+        {
+            "+" => firstNumber + secondNumber,
+            "-" => firstNumber - secondNumber,
+            "*" => firstNumber * secondNumber,
+            "/" => secondNumber != 0 ? firstNumber / secondNumber : double.NaN,
+            _ => double.NaN
+        };
+
+        if (double.IsNaN(result))
+        {
+            output.Content = "Error";
+        }
+        else
+        {
+            result = Math.Round(result, 4);
+            currentNumber = result.ToString();
+            output.Content = result.ToString();
+        }
         
-        output.Content = result.ToString();
+        isNewNumber = true;
     }
-    
-    public void zero(object sender, RoutedEventArgs e)
-    {
-        Console.WriteLine("0");
-    }
-    
-    public void one(object sender, RoutedEventArgs e)
-    {
-        Console.WriteLine("1");
-    }
-    
-    public void two(object sender, RoutedEventArgs e)
-    {
-        Console.WriteLine("2");
-    }
-    
-    public void three(object sender, RoutedEventArgs e)
-    {
-        Console.WriteLine("3");
-    }
-    
-    public void four(object sender, RoutedEventArgs e)
-    {
-        Console.WriteLine("4");
-    }
-    
-    public void five(object sender, RoutedEventArgs e)
-    {
-        Console.WriteLine("5");
-    }
-    
-    public void six(object sender, RoutedEventArgs e)
-    {
-        Console.WriteLine("6");
-    }
-    
-    public void seven(object sender, RoutedEventArgs e)
-    {
-        Console.WriteLine("7");
-    }
-    
-    public void eight(object sender, RoutedEventArgs e)
-    {
-        Console.WriteLine("8");
-    }
-    
-    public void nine(object sender, RoutedEventArgs e)
-    {
-        Console.WriteLine("9");
-    }
-
-    public void plus(object sender, RoutedEventArgs e)
-    {
-        Console.WriteLine("+");
-    }
-    
-    public void minus(object sender, RoutedEventArgs e)
-    {
-        Console.WriteLine("-");
-    }
-    
-    public void multiply(object sender, RoutedEventArgs e)
-    {
-        Console.WriteLine("*");
-    }
-
-    public void divide(object sender, RoutedEventArgs e)
-    {
-        Console.WriteLine("/");
-    }
-
-    public void equal(object sender, RoutedEventArgs e)
-    {
-        Console.WriteLine("=");
-    }
-
 }
