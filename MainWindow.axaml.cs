e0a5369a1c925e2edddd49759a207cc6bb3f9fb3
Ari Cummings <ari@aricummings.com>
2025-07-07 21:50:15 -0400

    UI done

--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -1,4 +1,6 @@
+using System;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 
 namespace dotnetCalculator;
 
@@ -8,8 +10,92 @@ public partial class MainWindow : Window
     {
         InitializeComponent();
     }
+
+    public void calculate(int num1, int num2, int operation)
+    {
+        double result = 0;
+
+
+
+
+
+        
+        output.Content = result.ToString();
+    }
     
+    public void zero(object sender, RoutedEventArgs e)
+    {
+        Console.WriteLine("0");
+    }
     
+    public void one(object sender, RoutedEventArgs e)
+    {
+        Console.WriteLine("1");
+    }
     
+    public void two(object sender, RoutedEventArgs e)
+    {
+        Console.WriteLine("2");
+    }
     
+    public void three(object sender, RoutedEventArgs e)
+    {
+        Console.WriteLine("3");
+    }
+    
+    public void four(object sender, RoutedEventArgs e)
+    {
+        Console.WriteLine("4");
+    }
+    
+    public void five(object sender, RoutedEventArgs e)
+    {
+        Console.WriteLine("5");
+    }
+    
+    public void six(object sender, RoutedEventArgs e)
+    {
+        Console.WriteLine("6");
+    }
+    
+    public void seven(object sender, RoutedEventArgs e)
+    {
+        Console.WriteLine("7");
+    }
+    
+    public void eight(object sender, RoutedEventArgs e)
+    {
+        Console.WriteLine("8");
+    }
+    
+    public void nine(object sender, RoutedEventArgs e)
+    {
+        Console.WriteLine("9");
+    }
+
+    public void plus(object sender, RoutedEventArgs e)
+    {
+        Console.WriteLine("+");
+    }
+    
+    public void minus(object sender, RoutedEventArgs e)
+    {
+        Console.WriteLine("-");
+    }
+    
+    public void multiply(object sender, RoutedEventArgs e)
+    {
+        Console.WriteLine("*");
+    }
+
+    public void divide(object sender, RoutedEventArgs e)
+    {
+        Console.WriteLine("/");
+    }
+
+    public void equal(object sender, RoutedEventArgs e)
+    {
+        Console.WriteLine("=");
+    }
+
 }
